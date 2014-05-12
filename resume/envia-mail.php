ac24f3cfd977f22b12bf7c1f6a9e27f78ff2bf60
Cabello, Hector Guillermo <kbzone.web@gmail.com>
2014-05-11 23:35:37 -0300

    Modificaciones de Contacto

--- resume/envia-mail.php
+++ resume/envia-mail.php
@@ -1,3 +1,7 @@
 <?php
-print_r($_POST);
-print_r($_GET);
+$cabeceras = "Content-type: text/html\r\n";
+$html = 'Motivo: ' . $_POST['subject'] . '<br />Nombre: ' . $_POST['name'] . '<br />E-mail: ' . $_POST['email'] . '<br />Mensaje:<br /><strong>' . $_POST['message'] . '</strong>';
+
+mail ('kbzone.web@gmail.com', 'Contacto desde CV', $html, $cabeceras);
+
+echo 'Gracias por contactarte! Te responderé a la brevedad.';
